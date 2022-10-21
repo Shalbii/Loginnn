274f4d2044ee9f1eb78d846d8b0023a30a89796b
Shalbii <shalbi.shaji@bizcloudexperts.com>
2022-10-21 13:21:57 +0530

    commit

--- src/Axios/Mainlist.js
+++ src/Axios/Mainlist.js
@@ -1,90 +1,4 @@
-// import "./Mainlist.css";
-// import React from "react";
-// import { AiOutlineArrowRight } from "react-icons/ai";
-// import { useState } from "react";
-// export default function Mainlist() {
-//   const [show, setShow] = useState(false);
-//   function HandleClick(){
-//     setShow(!show);
-//   }
-//   return (
-//     <>
-//       <div className="Mainlist_Outer">
-//         <div className="Mainlist_box"></div>
-//         <div className="Mainlist_header">
-//           <div className="Mainlist_row1">
-//             <ul>
-//               <li>FirstName</li>
-//               <li>LastName</li>
-              
-//               <li>Created on </li>
-//               <li>Email</li>
-//               {/* <li>Responses</li> */}
-//               <li>Status</li>
-//               <li>Owner</li>
-//             </ul>
-//           </div>
-//         </div>
-//         <div className="Mainlist_Innerbox">
-//           <div className="Mainlist_list">
-//             <div className="Mainlist_list_row_top">
-//               <input className="Cb" type="checkbox" onClick={HandleClick}/>
-//               <label for="checkbox"></label>
-//               <div className="Mainlist_list_row">
-//                 <label>John</label>
-//                 <label>Smith</label>
-                
-//                 <label>2022-02-01</label>
-//                 <label>larrywilson@nomail.com</label>
-//                 {/* <label>10</label> */}
-//                 <label>Confirmed</label>
-//                 <label>Larry wilson</label>
-//                 <div className="Mainlist_icon">
-//                   <AiOutlineArrowRight />
-//                 </div>
-//               </div>
-//             </div>
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <ListRow />
-//             <div>
-//               <button className="Mainlist_Button">load more leads</button>
-//             </div>
-//           </div>
-//           </div>
-//         </div>
-//     </>
-//   );
-// }
-// function ListRow() {
-//   return (
-//     <>
-//       <div className="Mainlist_list_row_top">
-//         <input className="Cb" type="checkbox"/>
-//               <label for="checkbox"></label>
-//         <div className="Mainlist_list_row">
-//           <label>John</label>
-//           <label>Smith</label>
-          
-//           <label>2022-02-01</label>
-//           <label>larrywilson@nomail.com</label>
-//           {/* <label>10</label> */}
-//           <label>Confirmed</label>
-//           <label>Larry wilson</label>
-//           <div className="Mainlist_icon">
-//             <AiOutlineArrowRight />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+
 
 
 import "./Mainlist.css";
@@ -151,6 +65,10 @@ function ListRow({itm, array, setArray}) {
     }
     setArray(temp)
   }
+  const d=new Date(itm.dtCreatedOn);
+  let day = d.getDay();
+  let month = d.getMonth();
+  let year = d.getFullYear();
   return (
     <>
       <div className={itm.isclicked?"Mainlist_list_row_topSelected":"Mainlist_list_row_top"} >
@@ -160,7 +78,8 @@ function ListRow({itm, array, setArray}) {
         <label>{itm.tasktitle}</label>
           <label>{itm.campaign}</label>
           <label>{itm.Status}</label>
-          <label>{itm.dtCreatedOn}</label>
+          <div>{day}-{month}-{year}</div>
+          
           <label>{itm.Leadname}</label>
         
           <label>{itm.owner}</label>
